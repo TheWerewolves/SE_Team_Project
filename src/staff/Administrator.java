4b1b2f8a58c8dfc9974a0e5a7ccd4b17834c0c1f
yuuto <yuutojurgen@gmail.com>
2020-02-28 00:08:32 +0000

    deleted irrelevant stuff

---
+++ src/staff/Administrator.java
@@ -0,0 +1,28 @@
+package staff;
+
+
+import java.util.ArrayList;
+
+import model.Database;
+import model.PTTClass;
+import model.Request;
+
+
+public class Administrator extends Staff {
+	
+	public Administrator(int id, String name, String password) {
+		super(id, name, password);
+	}
+	
+	public void organize(int requestIndex, int teacherID, PTTClass pttClass) {
+		Database database = Database.getInstance();
+		database.getRequestList().get(requestIndex).setTeacherID(teacherID);
+		database.getRequest(requestIndex).setPTTClass(pttClass);
+	}
+	
+	@Override
+	public ArrayList<Request> getRequest() {
+		Database db = Database.getInstance();
+		return db.getApprovedList();
+	}
+}
