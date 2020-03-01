240b621002b4dc8fd192da15d1cf0b4dcf08ab04
yuuto <yuutojurgen@gmail.com>
2020-03-01 01:38:55 +0000

    made changes

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,18 @@
 
+import model.Model;
 
+import view.Controller;
+import view.View;
 
 
 public class Client {
 	
 	public static void main(String[] args) {
+		
+		Model model = new Model();
+		Controller controller = new Controller(model);
+		View view = new View(model, controller);
+		controller.setView(view);
 		
 	}
 }
